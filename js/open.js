9954471baa2d07a8524662b94f9b6ccfe550947c
neapol2002 <73583307+neapol2002@users.noreply.github.com>
2023-04-29 16:16:29 +0400

    open js upd

--- js/open.js
+++ js/open.js
@@ -74,3 +74,38 @@ tabHeaders.forEach(function (item) {
 
     })
 })
+
+let elanClose = document.querySelectorAll('.ads_title_close');
+// console.log(elanClose);
+
+for (let i = 0; i < elanClose.length; i++) {
+    // console.log(i);
+    elanClose[i].onclick = tabsClose;
+}
+
+// let tabItems = document.querySelectorAll('.estate__config_column')
+function tabsClose() {
+for (let i = 0; i < contentBoxes.length; i++) {
+    // console.log(i);
+    if (contentBoxes[i].classList.contains('open')) {
+    contentBoxes[i].classList.remove("open");
+    // elanClose[i].onclick = closeTabs;
+    }
+}
+}
+
+// function closeTabs() {
+//     contentBoxes.classList.remove("open");
+// }
+
+// function closeTitle() {
+//     console.log('cliked');
+// }
+
+// for (let i = 0; i < elanClose.length; i++) {
+   
+//     elanClose[i].onclick = contentBoxes.forEach(function (item) {
+//             item.classList.remove('open');
+//         });
+//     // contentBoxes.classList.remove("open");
+// }
